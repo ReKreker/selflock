e86781f7825dd22ef0dee77956f87e16e39b82ab
ReKreker <ReKreker@uwu.com>
2023-12-19 14:53:06 +0300

    Refactoring code, more correct usage asserts, add deny ranges

--- main.c
+++ main.c
@@ -10,14 +10,18 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <signal.h>
+#include <sys/inotify.h>
+#include <sys/poll.h>
 
-#define SL_NEW0(obj, size) do{ obj = calloc(1,size); assert(obj); } while(0)
+#define SL_NEW0(obj, size) do{ obj = calloc(1,size); if(!obj){ puts("Memory allocation failed"); abort(); } } while(0)
 #define SL_FREE(obj) do{ free(obj); obj = NULL; }while(0)
 
+#define SL_RANGES_END {.from = NULL, .to = NULL}
+#define SL_RULES_END {.app = NULL}
+
 enum action {
-    //ACTION_NOTHING, // not implemented
     ACTION_ALLOW,
-    //ACTION_DISALLOW // not implemented
+    ACTION_DENY
 };
 
 // TODO: add weekend-based time ranges
@@ -41,26 +45,31 @@ struct sl_rule_t {
     struct sl_time_t time[MAX_TIME_RANGES]; // time ranges
 };
 
+// Allow by default. The last rules are more significant than the first ones
 static struct sl_rule_t rules[] = {
         [__COUNTER__] = {
                 .app = "Telegram",
                 .mt = MATCHTYPE_EXACT,
                 .time = {
-                        [0] = {.act = ACTION_ALLOW, .from = "13:37", .to = "14:37"},
-                        [1] = {.from = NULL, .to = NULL} // set "from" or "to" to NULL to mark end of time ranges
+                        [0] = {.act = ACTION_DENY, .from = "13:37", .to = "15:37"},
+                        [1] = SL_RANGES_END
                 }
         },
-        [__COUNTER__] = {.app = NULL} // mark end of rules
+        [__COUNTER__] = SL_RULES_END
 };
 
 int sl_selector(const struct dirent *d){
+    char letter;
+    const char *name;
+    char path[20];
+    struct stat stb;
+
     // filter non-dir
     if (d->d_type != DT_DIR)
         return 0;
 
     // filter non-numeric
-    char letter;
-    const char *name = d->d_name;
+    name = d->d_name;
     while ((letter = *name)){
         if (letter < '0' || '9' < letter)
             return 0;
@@ -68,13 +77,12 @@ int sl_selector(const struct dirent *d){
     }
 
     // filter dirs without current user creds
-    char path[20];
-    snprintf(path, 20, "/proc/%s/", d->d_name);
+    // TODO: find another way to check is process killable by current user
+    snprintf(path, 20-1, "/proc/%s/", d->d_name);
 
-    struct stat stb;
     if(stat(path, &stb)){
         printf("Failed stat for %s\n", path);
-        assert(0);
+        abort();
     }
 
     if (stb.st_uid != geteuid() && stb.st_gid != getegid())
@@ -85,7 +93,10 @@ int sl_selector(const struct dirent *d){
 
 int sl_enum_processes(struct dirent ***namelist){
     int res = scandir("/proc", namelist, sl_selector, alphasort);
-    assert(res > 0 && "Scandir error");
+    if (res <= 0){
+        puts("Scandir error for /proc\n");
+        abort();
+    }
     return res;
 }
 
@@ -96,12 +107,9 @@ int sl_match(const char *app_name, struct sl_rule_t *rule){
             if (!strcmp(app_name, rule->app))
                 return 0;
             break;
-        case MATCHTYPE_STARTS_WITH:
-            assert(0 && "Not implemented");
-        case MATCHTYPE_CONSIST:
-            assert(0 && "Not implemented");
         default:
-            assert(0 && "Not implemented");
+            puts("Not implemented");
+            abort();
     }
     return -1;
 }
@@ -113,7 +121,7 @@ void sl_get_app_name(char *app_name, const char *pid){
     int fd = open(path, O_RDONLY);
     if (fd == -1) {
         printf("Cannot open file from %s", path);
-        assert(0);
+        abort();
     }
     read(fd, path, 64); // reuse variable for path of executed bin
     close(fd);
@@ -122,60 +130,64 @@ void sl_get_app_name(char *app_name, const char *pid){
     strncpy(app_name, path, 64);
 }
 
-void sl_parse_time(const char *time_from_rule, long *hours, long *mins){
-    char *tmp;
-    errno = 0;
-    *hours = strtol(time_from_rule, &tmp, 10);
-    if (tmp[0] != ':') goto wrong_format;
-    *mins = strtol(&tmp[1], &tmp, 10);
-    if (errno) goto wrong_format;
+time_t sl_parse_time(struct tm base, const char *time_range){
+    int res = sscanf(time_range, "%d:%d", &base.tm_hour, &base.tm_min);
+    if (res != 2) {
+        printf("Wrong time format '%s' use something like '13:37'", time_range);
+        abort();
+    }
 
-    return;
-
-wrong_format:
-    printf("Wrong time format '%s' use something like '13:37'", time_from_rule);
-    assert(0);
+    time_t tmp = mktime(&base);
+    if (tmp == -1){
+        printf("Cannot convert time(%s) to epoch", time_range);
+        abort();
+    }
+    return tmp;
 }
 
 bool sl_is_allowed(struct sl_rule_t *rule){
     const time_t tt = time(NULL);
     struct tm *now = localtime(&tt);
+    time_t from_epoch, to_epoch, now_epoch = mktime(now);
 
-    bool allow_flag = false;
-    long f_hours, f_mins, t_hours, t_mins; // from & to int variables
+    bool allow_flag = true;
     int i = 0;
     struct sl_time_t t;
-    while ((t = rule->time[i], t.from || t.to)){
-        sl_parse_time(t.from, &f_hours, &f_mins);
-        sl_parse_time(t.to, &t_hours, &t_mins);
-        // TODO: rewrite this ugly logic
+
+    while ((t = rule->time[i], t.from && t.to)){
+        from_epoch = sl_parse_time(*now, t.from);
+        to_epoch = sl_parse_time(*now, t.to);
+        assert(from_epoch != -1 && to_epoch != -1);
         switch (t.act) {
             case ACTION_ALLOW:
-                if (f_hours < now->tm_hour && now->tm_hour < t_hours) {
+                if (from_epoch <= now_epoch && now_epoch <= to_epoch)
                     allow_flag = true;
-                } else if (f_hours == now->tm_hour && now->tm_hour < t_hours
-                        && f_mins <= now->tm_min) {
-                    allow_flag = true;
-                } else if (f_hours == now->tm_hour && now->tm_hour == t_hours
-                        && f_mins <= now->tm_min && now->tm_min <= t_mins){
-                    allow_flag = true;
-                } else if (f_hours < now->tm_hour && now->tm_hour == t_hours
-                           && now->tm_min <= t_mins){
-                    allow_flag = true;
-                }
+                break;
+            case ACTION_DENY:
+                if (from_epoch <= now_epoch && now_epoch <= to_epoch)
+                    allow_flag = false;
                 break;
             default:
-                assert(0 && "Not implemented");
+                puts("Not implemented");
+                abort();
         }
         i++;
+        assert(i <= MAX_TIME_RANGES && "Time ranges overflow - set SL_RANGES_END for last time range");
     }
     return allow_flag;
+}
+
+void sl_kill(char *pid_string){
+    pid_t pid = (pid_t) strtol(pid_string, NULL, 10);
+    kill(pid, SIGTERM);
+    sleep(1);
+    kill(pid, SIGKILL);
 }
 
 int sl_restrict(struct dirent ***namelist, int enum_amount){
     char app_name[64];
     for (unsigned int i = 0; ; ++i) {
-        assert(i < sizeof(rules)/sizeof(*rules) && "Rules overflow - set '.app = NULL' for last rule");
+        assert(i < sizeof(rules)/sizeof(*rules) && "Rules overflow - set SL_RULES_END for last rule");
         struct sl_rule_t *rule = &rules[i];
         if (rule->app == 0)
             break;
@@ -197,25 +209,25 @@ int sl_restrict(struct dirent ***namelist, int enum_amount){
 
         // killing denied
         printf("To kill: %s\n", app_name);
-        pid_t pid = (pid_t) strtol((*namelist)[j]->d_name, NULL, 10);
-        kill(pid, SIGTERM);
-        sleep(1);
-        kill(pid, SIGKILL);
-
+        sl_kill((*namelist)[j]->d_name);
     }
     return 0;
 }
 
 void sl_free_enum(struct dirent ***namelist, int enum_amount){
-    for (int i = 0; i < enum_amount; ++i)
+    for (int i = 0; i < enum_amount; ++i) {
         free((*namelist)[i]);
+        (*namelist)[i] = NULL;
+    }
     free(*namelist);
+    *namelist = NULL;
 }
 
 int main() {
     // TODO: add smth like poll to wait until new process in /proc
     struct dirent **namelist;
     int enum_amount = sl_enum_processes(&namelist);
+    assert(enum_amount > 0);
     sl_restrict(&namelist, enum_amount);
     sl_free_enum(&namelist, enum_amount);
 }
